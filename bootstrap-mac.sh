bea3004e44681126327034f6a4c56ad488b87ad5
Philip Brocoum <philip.brocoum@gmail.com>
2018-03-30 13:58:28 -0700

    refactor bootstrap-mac.sh plus completions

--- bootstrap-mac.sh
+++ bootstrap-mac.sh
@@ -1,36 +1,29 @@
-Bootstrap Mac
+# Bootstrap Mac
 
-Mac App Store: update and install
+# Mac App Store: update and install
 
 # Homebrew: https://brew.sh
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Base
-brew cask install bitwarden dropbox spotify iterm2
+# Base (with https://www.undercoverhq.com/)
+brew cask install bitwarden dropbox spotify iterm2 undercover
 
 # Safari
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2BackspaceKeyNavigationEnabled -bool YES
 
-# Undercover: https://www.undercoverhq.com/
-
 # Dropbox: Apps (macOS)
 
 # Terminal
-brew install tmux reattach-to-user-namespace bash liquidprompt ack gpg pinentry-mac
+brew install bash tmux reattach-to-user-namespace liquidprompt ack git gpg pinentry-mac
 
 # Dev
-brew install git chruby ruby-install node
+brew install chruby ruby-install node
 brew cask install visual-studio-code google-chrome imageoptim ngrok
 npm install -g browser-sync
 ruby-install ruby # Update .ruby-version
 gem install tmuxinator
 
-# Dropbox: Dotfiles (bashrc dln function)
-
-# Python (for tmuxp)
-easy_install --user pip
-pip install --user tmuxp
-
-# Bitwarden: My SSH & GPG Keys (also GitHub public key?)
+# Bitwarden: My SSH & GPG keys (also GitHub public key?)
 gpg --import [ example.key ]
 gpg --edit-key philip.brocoum@gmail.com
 > trust
@@ -38,9 +31,12 @@ gpg --edit-key philip.brocoum@gmail.com
 gpgconf --kill gpg-agent
 
 # Janus for Vim: https://github.com/carlhuda/janus
+curl -L https://bit.ly/janus-bootstrap | bash
 
 # Docker: https://store.docker.com
 brew cask install docker dockstation
 
 # Completions
-brew install bash-completion@2 tmuxinator-completion docker-completion docker-compose-completion docker-machine-completion
+brew install bash-completion@2 brew-cask-completion tmuxinator-completion gem-completion bundler-completion rake-completion rails-completion docker-completion docker-compose-completion docker-machine-completion
+
+# Dropbox: Dotfiles (link w/ bashrc dln function)
