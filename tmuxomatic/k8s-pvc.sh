7f2bb55157c50e1ce33e354625370eb13738fe3a
Philip Brocoum <philip.brocoum@gmail.com>
2018-05-18 09:30:27 -0700

    tmuxomatic/k8s-pvc.sh for --all-namespaces

--- tmuxomatic/k8s-pvc.sh
+++ tmuxomatic/k8s-pvc.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-kubectl get pvc --all-namespaces | ack -v kube | sed 's/\([A-Z][A-Z]\) \([A-Z][A-Z]\)/\1-\2/g' | awk 'NR<2 {$6=""; print $0; next} {printf "%s %s %s", $1, $2, substr($3, 1, 12); $1=$2=$3=$6=""; print $0}' | column -t
+kubectl get pvc --all-namespaces | sed 's/\([A-Z]\) \([A-Z]\)/\1-\2/g' | awk 'NR<2 {$7=""; print $0; next} {printf "%s %s %s %s %s %s %s\n", $1, $2, $3, substr($4, 1, 12), $5, $6, $8}' | column -t
 
