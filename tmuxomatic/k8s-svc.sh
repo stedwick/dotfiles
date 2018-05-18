5e0456c02d4c7ecb807b85791f726cd1e2f9ecfa
Philip Brocoum <philip.brocoum@gmail.com>
2018-05-18 08:08:04 -0700

    formatting kubectl get svc --all-namespaces

--- tmuxomatic/k8s-svc.sh
+++ tmuxomatic/k8s-svc.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-kubectl get svc --all-namespaces | ack -v kube | awk {'print $1" "$3" "$4" "$5'} | column -t
+kubectl get svc --all-namespaces | ack -v kube | awk {'print $1" "$2" "$5" "$6" "$7'} | column -t
 
