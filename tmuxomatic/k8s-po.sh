9748c9310e298dee85a37adce9328bfa379d843f
Philip Brocoum <philip.brocoum@gmail.com>
2018-05-18 22:45:26 -0700

    kube-tmux, ackrc, Quickfix, k8s.mux

--- tmuxomatic/k8s-po.sh
+++ tmuxomatic/k8s-po.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-kubectl get po --all-namespaces | ack -v kube | awk '{printf "%s ", substr($1, 1, 12); $1=""; print $0}' | column -t
+kubectl get po --all-namespaces | ack -v kube | awk '{printf "%s %s ", substr($1, 1, 12), substr($2, 1, 22); $1=$2=""; print $0}' | column -t
 
