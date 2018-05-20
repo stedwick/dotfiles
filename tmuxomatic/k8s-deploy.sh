0b2d4ecba86f997a08da2335b7ded823028fde48
Philip Brocoum <philip.brocoum@gmail.com>
2018-05-20 12:41:46 -0700

    tmuxomatic/k8s putting namespace back

--- tmuxomatic/k8s-deploy.sh
+++ tmuxomatic/k8s-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-# kubectl get deploy --all-namespaces | ack -v kube | awk '{printf "%s ", substr($1, 1, 12); $1=""; print $0}' | column -t
-kubectl get deploy --all-namespaces | ack -v kube | awk '{$1=""; print $0}' | column -t
+kubectl get deploy --all-namespaces | ack -v kube | awk '{printf "%s ", substr($1, 1, 12); $1=""; print $0}' | column -t
+# kubectl get deploy --all-namespaces | ack -v kube | awk '{$1=""; print $0}' | column -t
 
