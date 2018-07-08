dec41ea107556fc7d02c6095890ba6ba06246d9a
Philip Brocoum <philip.brocoum@gmail.com>
2018-07-07 19:29:48 -0700

    removing old files and making k8s directory

--- resume.sh
+++
@@ -1,6 +0,0 @@
-export RESUME_APP_ROOT="$RESUME_ROOT/resume-app"
-export RESUME_WEB_ROOT="$RESUME_ROOT/resume-web"
-export RESUME_DB_ROOT="$RESUME_ROOT/resume-db"
-
-[ -r "$HOME/.resume.secret.sh" ] && source "$HOME/.resume.secret.sh"
-
