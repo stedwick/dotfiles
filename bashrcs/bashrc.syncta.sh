1bb9e126eece868e77f4dd36a7c6a49b34004b3b
Philip Brocoum <philip.brocoum@gmail.com>
2018-07-15 18:35:33 -0700

    stagify and unstagify

--- bashrcs/bashrc.syncta.sh
+++ bashrcs/bashrc.syncta.sh
@@ -1,4 +1,17 @@
 export SYNCTA_ROOT="$DEV_ROOT/syncta"
 
+function stagify() {
+  export RDS_HOSTNAME=localhost
+  export RDS_PORT=5433
+}
+
+function unstagify() {
+  unset RDS_DB_NAME
+  unset RDS_USERNAME
+  unset RDS_PASSWORD
+  unset RDS_HOSTNAME
+  unset RDS_PORT
+}
+
 [ -r "$HOME/.bashrc.syncta.secret.sh" ] && source "$HOME/.bashrc.syncta.secret.sh"
 
