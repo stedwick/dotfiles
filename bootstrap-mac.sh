7004ea0a41c13a979f4c9b440d968784c0a5ed36
Philip Brocoum <philip.brocoum@gmail.com>
2018-08-17 18:27:57 -0700

    awscli and ultrahook

--- bootstrap-mac.sh
+++ bootstrap-mac.sh
@@ -19,7 +19,7 @@ brew install bash tmux reattach-to-user-namespace liquidprompt ack git tig gpg p
 # Powerline: Fonts -> Dotfiles/powerline (Inconsolata, 18pt, ~%110 vertical)
 
 # Dev (install Xcode from App Store)
-brew install chruby ruby-install node postgresql tmate wget git-flow gettext dnsmasq go colordiff pv yarn ctags
+brew install chruby ruby-install node postgresql tmate wget git-flow gettext dnsmasq go colordiff pv yarn ctags awscli
 brew cask install visual-studio-code google-chrome opera imageoptim ngrok slack macsvg docker virtualbox minikube postgres chromedriver
 npm install -g browser-sync
 ruby-install ruby # Update .ruby-version
@@ -32,6 +32,7 @@ brew install bower imagemagick phantomjs elasticsearch@2.4 aws-elasticbeanstalk
 # SSH keys
 # env.local
 # git config user.email "philip@c3backflow.com"
+# Ultrahook (Bitwarden)
 
 # minikube start
 # minikube addons enable metrics-server
