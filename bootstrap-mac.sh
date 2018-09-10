e376eee2cf3627b93757ac6c548733437272f6d0
Philip Brocoum <philip.brocoum@gmail.com>
2018-09-09 17:09:09 -0700

    Bootstrap Mac

--- bootstrap-mac.sh
+++ bootstrap-mac.sh
@@ -5,45 +5,82 @@
 # Homebrew: https://brew.sh
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Base (with https://www.undercoverhq.com/)
-brew cask install bitwarden dropbox spotify iterm2 firefox undercover caffeine keybase tunnelblick bowtie skype
-mkdir ~/bin
-# Miro http://www.mirovideoconverter.com/
-
-# Safari
+# Base
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2BackspaceKeyNavigationEnabled -bool YES
+mkdir ~/bin
+brew install bash figlet
+brew cask install bitwarden dropbox spotify iterm2 bowtie undercover keybase
+
+# Opera: https://www.opera.com
+# Page Shadow: https://addons.opera.com/en/extensions/details/page-shadow/
+# Postgres: https://postgresapp.com/
+# Redis: https://jpadilla.github.io/redisapp/
+# Join.me: https://www.join.me
+# Miro: http://www.mirovideoconverter.com/
+# Setapp: https://setapp.com
 
 # Dropbox: Apps (macOS)
 
 # Terminal
-brew install bash tmux reattach-to-user-namespace liquidprompt ack git tig gpg pinentry-mac vim tree htop glances hh
+brew install tmux reattach-to-user-namespace vim git tig liquidprompt ack hh tree gpg pinentry-mac # htop glances
 # Powerline: Fonts -> Dotfiles/powerline (Inconsolata, 18pt, ~%110 vertical)
 
 # Dev (install Xcode from App Store)
-brew install chruby ruby-install node postgresql tmate wget git-flow gettext dnsmasq go colordiff pv yarn ctags awscli
-brew cask install visual-studio-code google-chrome opera imageoptim ngrok slack macsvg docker virtualbox minikube postgres chromedriver skype-for-business
-npm install -g browser-sync
+brew install ruby-install chruby node yarn postgresql wget ctags imagemagick awscli # dnsmasq go git-flow tmate colordiff pv gettext
+brew cask install visual-studio-code sourcetree gpg-suite slack google-chrome firefox skype imageoptim ngrok virtualbox # docker minikube macsvg
+# npm install -g bower
+pip3 install --user tmuxp
+pip3 install --user powerline-status
 ruby-install ruby # Update .ruby-version
-go get github.com/jsha/minica
+
+# Completions
+brew install bash-completion@2 brew-cask-completion gem-completion bundler-completion rake-completion rails-completion spring-completion # docker-completion docker-compose-completion docker-machine-completion
+
+# Janus for Vim: https://github.com/carlhuda/janus
+curl -L https://bit.ly/janus-bootstrap | bash
 
 # Syncta
-brew cask install homebrew/cask-versions/java8 postgres sourcetree redis-app joinme gpg-suite
-brew install bower imagemagick phantomjs elasticsearch@2.4 aws-elasticbeanstalk
-# AWS CLI
+brew cask install homebrew/cask-versions/java8
+brew install phantomjs elasticsearch@2.4 # aws-elasticbeanstalk
+brew cask install chromedriver skype-for-business onedrive
+
 # SSH keys
-# env.local
-# git config user.email "philip@c3backflow.com"
+# GPG keys (GPG Keychain)
+
+# Syncta
+# AWS CLI: aws configure
+mkdir ~/dev
+git clone git@bitbucket.org:jeremiahchurch/ccc.git ~/dev/syncta
+cd ~/dev/syncta
+# gem pristine --all
+git config user.email "philip@c3backflow.com"
+cat README.md
 # Ultrahook (Bitwarden)
+# env.local
+# OneDrive: .git/hooks/
+# OneDrive shared sync: https://login.microsoftonline.com
 
-# minikube start
-# minikube addons enable metrics-server
-# minikube addons enable heapster
+# Dropbox: Dotfiles (link w/ bashrc dln function)
+# ? .bashrc.syncta.secret.sh
+# $HOME/Library/Application\ Support/Code/User/settings.json
 
-# ?
-brew install python3
-# pip3 install tmuxomatic
-pip install --user tmuxp
-pip3 install powerline-status
+#####
+#####
+#####
+
+# nom install -g browser-sync
+
+# GitHub public key?
+
+# GPG
+gpg --import [ example.key ]
+gpg --edit-key philip.brocoum@gmail.com
+> trust
+> 5
+gpgconf --kill gpg-agent
+
+# Dash: https://kapeli.com/dash
+# MySQL: https://www.mysql.com/
 
 # DNSMasq
 sudo bash -c 'echo "address=/.localhost/127.0.0.1" > /usr/local/etc/dnsmasq.conf'
@@ -51,30 +88,15 @@ sudo bash -c 'echo "address=/.kube/192.168.99.100" > /usr/local/etc/dnsmasq.conf
 sudo /usr/local/sbin/dnsmasq
 sudo killall -HUP mDNSResponder
 # Edit DNS in System Prefs, add 127.0.0.1 and 8.8.8.8
+go get github.com/jsha/minica
 
-# Dash: https://kapeli.com/dash (Setapp?)
-# MySQL: https://www.mysql.com/
-
-# Bitwarden: My SSH & GPG keys (also GitHub public key?)
-gpg --import [ example.key ]
-gpg --edit-key philip.brocoum@gmail.com
-> trust
-> 5
-gpgconf --kill gpg-agent
-
-# Janus for Vim: https://github.com/carlhuda/janus
-curl -L https://bit.ly/janus-bootstrap | bash
-
-# Ruby on Rails (Unneeded thx to Docker?)
-gem install rails # `gem pristine --all` if needed
-
-# Completions
-brew install bash-completion@2 brew-cask-completion gem-completion bundler-completion rake-completion rails-completion spring-completion docker-completion docker-compose-completion docker-machine-completion
-
-# Dropbox: Dotfiles (link w/ bashrc dln function)
-# .bashrc.resume.secret.sh .bashrc.syncta.secret.sh
+# Docker
 ln -s ~/Dropbox/Dotfiles/docker-dashboard/sort_with_header.sh ~/bin/
 
-# .bashrc.default.secret.sh .bashrc.resume.secret.sh .bashrc.syncta.secret.sh
-# ~/dev/default-lb/certs
+# minikube start
+# minikube addons enable metrics-server
+# minikube addons enable heapster
 
+# Resume
+# .bashrc.default.secret.sh .bashrc.resume.secret.sh
+# ~/dev/default-lb/certs
